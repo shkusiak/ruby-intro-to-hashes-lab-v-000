4b6471cddd6208946b7f3426984b01691e23ff7e
Shea Kusiak <shkusiak@gmail.com>
2018-02-22 04:30:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 				:four_pieces_owned => 200},
 			:names => {
 				:reading_railroad => {},
-				:pennslvania_railroad => {},
+				:pennsylvania_railroad => {},
 				:b_and_o_railroad => {},
 				:shortline => {}
 				}}
