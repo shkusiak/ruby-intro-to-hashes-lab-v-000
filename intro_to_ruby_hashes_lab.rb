9670e9e5b96d4a026c432687b7ae0306e584d11c
Shea Kusiak <shkusiak@gmail.com>
2018-02-22 04:24:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,8 @@ def monopoly_with_third_tier
 			:pieces => 4,
 			:names => {},
 			:rent_in_dollars => {:one_piece_owned => 25,
-				:rent_in_dollars => {:two_pieces_owned => 50}}
-		}}
+				:two_pieces_owned => 50}}
+		}
 end
 
 def monopoly_with_fourth_tier
